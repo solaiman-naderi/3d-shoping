1452e001bbb27ad6ac78029a08cab468679cf494
solaiman-naderi <solaimannaderi00@gmail.com>
2024-04-03 09:03:47 +0330

    added tow more components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlight";
 import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWork";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
     </main>
   );
 };
